c5fabb86e02a1b141eec4f85f1d6672fa237e95f
Mauricio Pajes <mpajes2006@gmail.com>
2026-06-03 17:23:34 -0500

    chore: add Spring Boot platform scaffold

---
+++ src/main/java/com/acme/coldtrace/platform/ColdtracePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.coldtrace.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdtracePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdtracePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/coldtrace/platform/ColdtracePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.coldtrace.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdtracePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
